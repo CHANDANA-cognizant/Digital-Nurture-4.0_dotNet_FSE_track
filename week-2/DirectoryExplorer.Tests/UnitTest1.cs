039abebf95eac91d32d30ebe77ec10463ba4d01a
CHANDANA-cognizant <anuchandu2012@gmail.com>
2025-06-29 23:58:40 +0530

    Add files via upload

---
+++ week-2/DirectoryExplorer.Tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Moq;
+using MagicFilesLib;
+using System.Collections.Generic;
+
+namespace DirectoryExplorer.Tests
+{
+    [TestFixture]
+    public class DirectoryExplorerTests
+    {
+        private readonly string _file1 = "file.txt";
+        private readonly string _file2 = "file2.txt";
+
+        [Test]
+        public void GetFiles_ReturnsExpectedFiles()
+        {
+            // Arrange
+            var mock = new Mock<IDirectoryExplorer>();
+            mock.Setup(x => x.GetFiles(It.IsAny<string>()))
+                .Returns(new List<string> { _file1, _file2 });
+
+            // Act
+            var result = mock.Object.GetFiles("dummy_path");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count);
+            CollectionAssert.Contains(result, _file1);
+        }
+    }
+}
