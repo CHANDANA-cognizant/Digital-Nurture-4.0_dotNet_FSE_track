4dfe0af92f4a69cb46373461b20eb2409bdb2329
CHANDANA-cognizant <anuchandu2012@gmail.com>
2025-08-03 21:45:27 +0530

    Add files via upload

---
+++ BookDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function BookDetails() {
+  return (
+    <div>
+      <h2>📚 Book Details</h2>
+      <p>Title: Clean Code</p>
+      <p>Author: Robert C. Martin</p>
+      <p>Genre: Software Engineering</p>
+    </div>
+  );
+}
