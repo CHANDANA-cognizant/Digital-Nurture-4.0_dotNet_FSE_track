bca9d2527b2397a2f5c9893a99970b0643bcb7e2
CHANDANA-cognizant <anuchandu2012@gmail.com>
2025-08-03 22:36:23 +0530

    Add files via upload

---
+++ week6&7/App.js
@@ -0,0 +1,15 @@
+import { ScoreCalculator } from './components/ScoreCalculator';
+
+function App() {
+  return (
+    <div >
+      <ScoreCalculator Name={"chandana"}
+      School={"vignana bharathi"}
+      total={589}
+      goal={6}/>
+      
+    </div>
+  );
+}
+
+export default App;
